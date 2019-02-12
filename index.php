788ea26381d84a1769d389d8c514f8695ea78af7
Jacky LIM <jacky.lim@edu.itescia.fr>
2019-02-12 14:33:18 +0100

    Creation de l'espace membre => connexion/inscription  // probleme avec l'accès au serveur

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <meta charset="UTF-8">
 <link rel="stylesheet" href="blocnote.css">
+<!--
 <html>
     <head>
         <title>Yet Another Notepad</title>
     </head>
     <body>
         <h1>Yet Another NotePad</h1>
- <!-- Page d'accueil à l'entrée du site -->
+ //// Page d'accueil à l'entrée du site 
         <div class="homePage">
             <section class="head">
                 <h2>Présentation:</h2>
@@ -19,7 +20,7 @@
             <section>
         </div>
 
-<!-- formulaire de connexion quand on appuis sur le bouton connexion -->
+//// formulaire de connexion quand on appuis sur le bouton connexion 
         <div class="login-content">
             <div class="login-header">
                 <button type="button" class="close" data-dismiss="login">×</button>
@@ -48,10 +49,61 @@
 				</div>
 			</form>
         </div>
+</html> -->
 
-<!-- Page principale d'accès aux note -->
-        <div class="mainPage">
-        </div>
-    </body>
-    <script src=''></script>
+
+<?php
+// on teste si le visiteur a soumis le formulaire de connexion
+if (isset($_POST['connexion']) && $_POST['connexion'] == 'Connexion') {
+	if ((isset($_POST['email']) && !empty($_POST['email'])) && (isset($_POST['pass']) && !empty($_POST['pass']))) {
+
+	$base = mysql_connect ('serveur', 'email', 'password');
+	mysql_select_db ('blocnotedatabase', $base);
+
+	// on teste si une entrée de la base contient ce couple login / pass
+	$sql = 'SELECT count(*) FROM membre WHERE email="'.mysql_escape_string($_POST['email']).'" AND pass_md5="'.mysql_escape_string(md5($_POST['pass'])).'"';
+	$req = mysql_query($sql) or die('Erreur SQL !<br />'.$sql.'<br />'.mysql_error());
+	$data = mysql_fetch_array($req);
+
+	mysql_free_result($req);
+	mysql_close();
+
+	// si on a une réponse, alors l'utilisateur est un membre et on le redirige vers la page des notes
+	if ($data[0] == 1) {
+		session_start();
+		$_SESSION['email'] = $_POST['email'];
+		header('Location: YANnote.php');
+		exit();
+	}
+	// si rien ne concorde, le visiteur s'est trompé dans son login ou dans son mot de passe
+	elseif ($data[0] == 0) {
+		$erreur = 'Le compte n\'est pas reconnu';
+	}
+	// cas rare/exceptionnel du membre au identifiants identiques à d'autre
+	else {
+		$erreur = 'Probème dans la base de données : plusieurs membres ont les mêmes identifiants de connexion.';
+	}
+	}
+	else {
+	$erreur = 'Au moins un des champs est vide.';
+	}
+}
+?>
+<html>
+<head>
+<title>Accueil YAN</title>
+</head>
+
+<body>
+Connexion à l'espace membre :<br />
+<form action="index.php" method="post">
+Email : <input type="text" name="email" value="<?php if (isset($_POST['email'])) echo htmlentities(trim($_POST['email'])); ?>"><br />
+Mot de passe : <input type="password" name="pass" value="<?php if (isset($_POST['pass'])) echo htmlentities(trim($_POST['pass'])); ?>"><br />
+<input type="submit" name="connexion" value="Connexion">
+</form>
+<a href="signup.php">Vous inscrire</a>
+<?php
+if (isset($erreur)) echo '<br /><br />',$erreur;
+?>
+</body>
 </html>
--- signup.php
+++ signup.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <meta charset="UTF-8">
 <link rel="stylesheet" href="blocnote.css">
+<!--
 <html>
     <head>
         <title>Yet Another Notepad</title>
@@ -8,9 +9,9 @@
     <body>
         <h1>Yet Another NotePad</h1>
 
-<!-- Page de création de compte -->
+// Page de création de compte 
         <div class="accountCreationPage">
-<!-- Formulaire de création de compte -->
+// Formulaire de création de compte 
             <form action="" method="post">
                 <div>
                     <label for="lastName">Nom :</label>
@@ -35,4 +36,70 @@
         </div>
     </body>
     <script src=''></script>
+</html>
+-->
+//////////////////////////////////////////////////////////
+<?php
+// on teste si le visiteur a soumis le formulaire
+if (isset($_POST['inscription']) && $_POST['inscription'] == 'Je m\'inscris') {
+	// on vérifie l'existence des variables et si elles ne sont pas vides
+    if ((isset($_POST['lastName']) && !empty($_POST['lastName']))&&
+        (isset($_POST['firstName']) && !empty($_POST['firstName']))&&
+        (isset($_POST['email']) && !empty($_POST['email'])) && 
+        (isset($_POST['pass']) && !empty($_POST['pass'])) && 
+        (isset($_POST['pass_confirm']) && !empty($_POST['pass_confirm']))) {
+	// on vérifie la cohérence des 2 mots de passe
+	if ($_POST['pass'] != $_POST['pass_confirm']) {
+		$erreur = 'Les 2 mots de passe sont différents.';
+	}
+	else {
+		$base = mysqli_connect ('localhost:8000', 'email', 'password');
+		mysqli_select_db ('blocnotedatabase', $base);
+
+		// on vérifie que ce login n'est pas déjà utilisé par quelqu'un d'autre
+		$sql = 'SELECT count(*) FROM member WHERE email="'.mysqli_escape_string($_POST['email']).'"';
+		$req = mysqli_query($sql) or die('Erreur SQL !<br />'.$sql.'<br />'.mysqli_error());
+		$data = mysqli_fetch_array($req);
+
+		if ($data[0] == 0) {
+        $sql = 'INSERT INTO member VALUES("", "'.mysqli_escape_string($_POST['lastName']).'",
+                                            "'.mysqli_escape_string($_POST['firstName']).'",
+                                            "'.mysqli_escape_string($_POST['email']).'",
+                                            "'.mysqli_escape_string(md5($_POST['pass'])).'")';
+		mysqli_query($sql) or die('Erreur SQL !'.$sql.'<br />'.mysqli_error());
+
+		session_start();
+		$_SESSION['email'] = $_POST['email'];
+		header('Location: index.php');
+		exit();
+		}
+		else {
+		$erreur = 'Un membre possède déjà ce login.';
+		}
+	}
+	}
+	else {
+	$erreur = 'Au moins un des champs est vide.';
+	}
+}
+?>
+<html>
+<head>
+<title>Inscription YAN</title>
+</head>
+
+<body>
+Inscription à l'espace membre de YAN :<br />
+<form action="signup.php" method="post">
+Nom: <input type="text" name="lastName" value="<?php if (isset($_POST['lastName'])) echo htmlentities(trim($_POST['lastName'])); ?>" placeholder="Votre Nom"><br />
+Prénom: <input type="text" name="firstName" value="<?php if (isset($_POST['firstName'])) echo htmlentities(trim($_POST['firstName'])); ?>" placeholder="Votre Prénom"><br />
+E-mail : <input type="text" name="email" value="<?php if (isset($_POST['email'])) echo htmlentities(trim($_POST['email'])); ?>" placeholder="Votre Email"><br />
+Mot de passe : <input type="password" name="pass" minlength="8" value="<?php if (isset($_POST['pass'])) echo htmlentities(trim($_POST['pass'])); ?>"  placeholder="Votre mot de passe"><br />
+Confirmation du mot de passe : <input type="password" name="pass_confirm" value="<?php if (isset($_POST['pass_confirm'])) echo htmlentities(trim($_POST['pass_confirm'])); ?>" placeholder="Confirmation du mot de passe"><br />
+<input type="submit" name="inscription" value="Je m'inscris">
+</form>
+<?php
+if (isset($erreur)) echo '<br />',$erreur;
+?>
+</body>
 </html>
