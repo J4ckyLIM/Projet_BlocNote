fe76c1f0ce691520249ffcecedb4b355c9d78ae1
Jacky LIM <jacky.lim@edu.itescia.fr>
2019-02-18 20:41:10 +0100

    Ajout style sur notePage.php

--- view/notePage.php
+++ view/notePage.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <meta charset="UTF-8">
-<link rel="stylesheet" href="blocnote.css">
+<link rel="stylesheet" href="../css/blocnote.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 <?php
 require_once "../class/noteManager.php";
 
 session_start();
 // On vérifie que les champs sont tous remplies au moment d'appuyer sur le bouton 'Enregistrer' 
-if (isset($_POST['save']) && $_POST['save'] == 'save') {
+if (isset($_POST['save']) && $_POST['save'] == 'Enregistrer') {
     if ((isset($_POST['title']) && !empty($_POST['title'])) &&
         (isset($_POST['description']) && !empty($_POST['description'])) && 
         (isset($_POST['content']) && !empty($_POST['content']))) {
@@ -30,37 +30,24 @@ if (isset($erreur)) echo '<br />',$erreur;
         <title>Yet Another NotePad</title>
     </head>
     <body>
-        <h1>Yet Another NotePad</h1>
-
 <!-- Page principale d'accès aux note -->
-        <div class="mainPage">
-            <form action="notePage.php" method="post">
-                <div>
+                <form class="form-note" action="notePage.php" method="post">
+                <h1>Yet Another NotePad</h1>
                     <label for="noteTitle">Titre :</label>
-                    <input type="text" id="noteTitle" name="title" value="<?php if (isset($_POST['title'])) echo htmlentities(trim($_POST['title'])); ?>" placeholder="Titre de la note">
-                </div>
-                <div>
+                    <input class="noteData" type="text" id="noteTitle" name="title" value="<?php if (isset($_POST['title'])) echo htmlentities(trim($_POST['title'])); ?>" placeholder="Titre de la note">
+              
+                
                     <label for="noteDescription">Description :</label>
-                    <input id="text" id="noteDescription" name="description" value="<?php if (isset($_POST['description'])) echo htmlentities(trim($_POST['description'])); ?>" placeholder="Description de la note (20 caractères maximum)" maxlength="20">
-                </div>
-<!-- Barre d'outil tel que la fonction gras/italic/souligner...-->
-               <!-- <div class="toolbar" role="Barre d'outil">
-                <a id="grasButton" class="toolButton" href="javascript:void('Gras')" title="Gras" role="button"><i class="fas fa-bold"></i></a>
-                <a id="italicButton" class="toolButton" href="javascript:void('Italique')" title="Italique" role="button"><i class="fas fa-italic"></i></a>
-                <a id="underlineButton" class="toolButton" href="javascript:void('Souligné')" title="Souligné" role="button"><i class="fas fa-underline"></i></a>
-                <a id="strikeButton" class="toolButton" href="javascript:void('Barré')" title="Barré" role="button"><i class="fas fa-strikethrough"></i></a>
-                <a id="olListButton" class="toolButton" href="javascript:void('Insérer/Supprimer la liste numérotée')" title="Liste numérotée" role="button"><i class="fas fa-list-ul"></i></a>
-                <a id="olListButton" class="toolButton" href="javascript:void('Insérer/Supprimer la liste numérotée')" title="Liste numérotée" role="button"><i class="fas fa-list-ol"></i></a>
-                </div>  -->
+                    <input class="noteData" id="text" id="noteDescription" name="description" value="<?php if (isset($_POST['description'])) echo htmlentities(trim($_POST['description'])); ?>" placeholder="Description de la note (20 caractères maximum)" maxlength="20">
+               
                 <div>
-                        <textarea id="noteContent" name="content" value="<?php if (isset($_POST['content'])) echo htmlentities(trim($_POST['content'])); ?>" placeholder="Ecrivez votre note ici"></textarea>
-                    </div>
-                <div class="button">
-                    <button type="submit" name="save" value="save">Enregistrer</button>
-                    <a href ="noteListe.php"> <button type="button" value="cancel">Annuler</button></a>
+                        <textarea class="noteContent" id="noteContent" name="content" value="<?php if (isset($_POST['content'])) echo htmlentities(trim($_POST['content'])); ?>" placeholder="Ecrivez votre note ici"></textarea>
+                </div>
+                <div>
+                    <input class="button" type="submit" name="save" value="Enregistrer">
+                    <a href ="noteListe.php"> <button class="button" type="button" value="cancel">Annuler</button></a>
                 </div>
             </form>
-        </div>
     </body>
     <script src=''></script>
 </html>
